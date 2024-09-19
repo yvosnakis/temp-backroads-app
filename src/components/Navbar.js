bb3793c6614d13019e6d5ff57a844e3ea2e56240
Yannis Vosnakis <yannisvosnakis@Yanniss-MacBook-Air.local>
2024-09-19 15:56:46 +0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
+import { socialLinks } from "../data";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a href={href} className={icon}>
+                  {" "}
+                  home{" "}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
